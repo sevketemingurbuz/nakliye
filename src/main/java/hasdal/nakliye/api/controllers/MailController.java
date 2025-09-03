d1e91da4a36252f5fe5938fef14389186704115d
sevketemingurbuz <sevketemingurbuz6@gmail.com>
2025-09-03 14:46:06 +0300

    .env

--- src/main/java/hasdal/nakliye/api/controllers/MailController.java
+++ src/main/java/hasdal/nakliye/api/controllers/MailController.java
@@ -8,7 +8,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import hasdal.nakliye.business.concretes.MailService;
+<<<<<<< HEAD
 import hasdal.nakliye.entities.MailDto;
+=======
+import hasdal.nakliye.entities.MailRequest;
+>>>>>>> 200cca3 (.env)
 import jakarta.validation.Valid;
 
 @CrossOrigin(origins = "http://localhost:3000")
@@ -23,11 +27,19 @@ public class MailController {
 	}
 	
 	@PostMapping("/send")
+<<<<<<< HEAD
 	public ResponseEntity<String> send(@RequestBody @Valid MailDto mailRequest) {
 		String text = mailRequest.getMessage()
 				+ "\n\nGönderici: " + mailRequest.getName()
 				+"\n" + mailRequest.getPhone();//4.45-6.05 6.35-7.35 9.37-11.22 /////3.59
 		mailService.sendMail(text, mailRequest.getSubject());
+=======
+	public ResponseEntity<String> send(@RequestBody @Valid MailRequest mailDto) {
+		String text = mailDto.getMessage()
+				+ "\n\nGönderici: " + mailDto.getName()
+				+"\n" + mailDto.getPhone();//4.45-6.05 6.35-7.35 9.37-11.22 /////3.59
+		mailService.sendMail(text, mailDto.getSubject());
+>>>>>>> 200cca3 (.env)
 		return ResponseEntity.ok("Mail başarıyla gönderildi");
 	}
 	
