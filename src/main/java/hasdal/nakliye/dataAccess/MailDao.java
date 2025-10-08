1957d74e0af9325666fa86c123910fd23e088afd
sevketemingurbuz <sevketemingurbuz6@gmail.com>
2025-10-08 12:01:30 +0300

    removed round file

--- src/main/java/hasdal/nakliye/dataAccess/MailDao.java
+++
@@ -1,9 +0,0 @@
-package hasdal.nakliye.dataAccess;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import hasdal.nakliye.entities.Mail;
-
-public interface MailDao extends JpaRepository<Mail, Integer>{
-
-}
